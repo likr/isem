092c84fc773ccd64702bd285254bf3754cba4587
armorik83 <okunokentaro@me.com>
2015-01-28 15:12:36 +0900

    typing(egrid-sem): add interfaces

--- app/src/scripts/egrid-sem.ts
+++ app/src/scripts/egrid-sem.ts
@@ -1,9 +1,101 @@
-// from lib/egrid.js
-declare var egrid: any;
+module Svg {
+  export declare class Point {
+    x: number;
+    y: number;
+  }
+}
 
-// from http://hyperinfo.viz.media.kyoto-u.ac.jp/wsgi/websem/static/sem.js
-declare var sem: (n: any, alpha: any, sigma: any, S: any, callback: Function) => void;// from http://hyperinfo.viz.media.kyoto-u.ac.jp/wsgi/websem/static/sem.js
+module egrid {
+  /**
+   * @see {@link https://github.com/likr/egrid-core/blob/cce2cd81a30c098cb5d4a4cd38be92be5105b941/ts/egrid/grid.ts#L8}
+   */
+  export interface NodeInstance {
+    index : number;
+    x : number;
+    y : number;
+    baseWidth : number;
+    baseHeight : number;
+    width : number;
+    height : number;
+    theta : number;
+    text : string;
+    dagre : any;
+    weight : number;
+    key : number;
+    original : boolean;
+    isTop : boolean;
+    isBottom : boolean;
+    active : boolean;
+    participants : string[];
+  }
+
+  export interface NodeConstructor {
+    new(text: string, weight?: number, original?: boolean, participants?: string[]): NodeInstance;
+  }
+
+  /**
+   * @see {@link https://github.com/likr/egrid-core/blob/cce2cd81a30c098cb5d4a4cd38be92be5105b941/ts/egrid/grid.ts#L82}
+   */
+  export interface LinkInstance {
+    index : number;
+    points : Svg.Point[];
+    previousPoints : Svg.Point[];
+    dagre : any;
+    weight : number;
+    key : number;
+    source: NodeInstance;
+    target: NodeInstance;
+  }
+
+  export interface LinkConstructor {
+    new(source: NodeInstance, target: NodeInstance, weight?: number): LinkInstance;
+  }
+
+  /**
+   * @see {@link https://github.com/likr/egrid-core/blob/cce2cd81a30c098cb5d4a4cd38be92be5105b941/ts/egrid/sem.ts}
+   */
+  export interface SEMInstance {
+    // extends from egrid.DAG
+    nodes(): NodeInstance[];
+    nodes(nodes: NodeInstance[]): SEMInstance;
+
+    links() : LinkInstance[];
+    links(links: LinkInstance[]): SEMInstance;
+
+    registerUiCallback(callback: () => void): SEMInstance;
+    display(regionWidth?: number, regionHeight?: number): (selection: D3.Selection) => void
+
+    // declaration by egrid.SEM
+    activeNodes(): NodeInstance[];
+    activeLinks(): LinkInstance[];
+    draw(): egrid.SEMInstance;
+    focusCenter(): egrid.SEMInstance;
+  }
+
+}
+
+/**
+ * from root/app/lib/egrid.js
+ */
+declare var egrid: {
+  sem(): egrid.SEMInstance;
+  Node: egrid.NodeConstructor;
+  Link: egrid.LinkConstructor;
+};
+
+/**
+ * @see {@link http://hyperinfo.viz.media.kyoto-u.ac.jp/wsgi/websem/static/sem.js}
+ */
+declare var sem: (n: number, alpha: any, sigma: any, S: any, callback: Function) => void;
 declare var cov: (data: any, callback: Function) => void;
+
+declare var typeSDict: {
+  [node1Text: string]: {
+    [node2Text: string]: any
+  }
+};
+
+declare var typeNodesDict: {[nodeText: string]: number};
 
 interface Scope extends ng.IScope {
   gfiValue: any;
@@ -16,7 +108,7 @@ interface Scope extends ng.IScope {
 angular.module('egrid-sem', [])
   .controller('SemController', ['$scope', function($scope: Scope) {
     var dag = egrid.sem();
-    var SDict: any;
+    var SDict: typeof typeSDict;
 
     /**
      * @returns {void}
@@ -40,6 +132,7 @@ angular.module('egrid-sem', [])
           return SDict[node1.text][node2.text];
         });
       });
+
       sem(n, alpha, sigma, S, (function(result: any) {
         var A = nodes.map(function(_: any) {
           return nodes.map(function(_: any) {
