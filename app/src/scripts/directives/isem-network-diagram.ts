5d458a5dde2ab77fbd8607f0bd945dee2654fc09
armorik83 <okunokentaro@me.com>
2015-02-20 21:23:28 +0900

    feat(isem-network-diagram): implement controller

--- app/src/scripts/directives/isem-network-diagram.ts
+++ app/src/scripts/directives/isem-network-diagram.ts
@@ -1,11 +1,36 @@
 'use strict';
 import angular = require('angular');
 import app = require('../app');
+import vas = require('../services/variable-array-store');
+
+class NetworkDiagramController {
+  /**
+   * @constructor
+   * @ngInject
+   */
+  constructor(
+    private $rootScope: ng.IRootScopeService,
+    private VariableArrayStore: vas
+  ) {
+    this.subscribe();
+  }
+
+  /**
+   * @returns {void}
+   */
+  private subscribe() {
+    this.$rootScope.$on('isem:addVariable', (e, arg) => {
+      this.VariableArrayStore.addVariable(arg);
+    })
+  }
+}
 
 function NetworkDiagramDDO() {
   return {
     restrict: 'E',
-    templateUrl: 'src/views/isem-network-diagram.html'
+    templateUrl: 'src/views/isem-network-diagram.html',
+    controller: NetworkDiagramController,
+    controllerAs: 'NetworkDiagram'
   }
 }
 
