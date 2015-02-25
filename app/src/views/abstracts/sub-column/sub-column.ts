4c73482b884c792ad27c87b0cf4bd9a83220d9db
armorik83 <okunokentaro@me.com>
2015-02-25 16:59:39 +0900

    refactor(isem-sub-column): move directory

--- app/src/views/abstracts/sub-column/sub-column.ts
+++ app/src/views/abstracts/sub-column/sub-column.ts
@@ -1,6 +1,6 @@
 'use strict';
 import angular = require('angular');
-import app = require('../app');
+import app = require('../../../scripts/app');
 
 function styling(tElement: ng.IAugmentedJQuery) {
   tElement
