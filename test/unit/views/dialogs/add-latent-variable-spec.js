8f95b39e0929b68ff67d35762c12a056113d6b12
armorik83 <okunokentaro@me.com>
2015-02-26 14:22:40 +0900

    test(add-latent-variable): add tests

--- test/unit/views/dialogs/add-latent-variable-spec.js
+++ test/unit/views/dialogs/add-latent-variable-spec.js
@@ -1,18 +1,83 @@
 'use strict';
-var powerAssert = require('power-assert');
-var assert = powerAssert.customize({output: {maxDepth: 2}});
+var assert = require('power-assert').customize({output: {maxDepth: 2}});
 var sinon = require('sinon');
 
-var Injector = require('../../../../app/src/scripts/injector');
-var stub = {
-  app:     sinon.stub(Injector, 'app').returns({}),
-  angular: sinon.stub(Injector, 'angular').returns({'module': () => {return {directive: () => {}}}})
-};
+var stub = (() => {
+  var Injector = require('../../../../app/src/scripts/injector');
+  var mockAngular = {'module': () => {
+    return {
+      config: () => {},
+      directive: () => {}
+    };
+  }};
+  return {
+    angular: sinon.stub(Injector, 'angular').returns(mockAngular)
+  }
+})();
 var AddLatentVariable = require('../../../../app/src/views/concretes/dialogs/add-latent-variable/add-latent-variable');
+var ControllerStatic = AddLatentVariable.Controller;
 
+var mockRootScope, stubRootScope, mockScope, stubScope;
+var Controller = (() => {
+  mockRootScope = {
+    $broadcast: () => {}
+  };
+  stubRootScope = {
+    $broadcast: sinon.stub(mockRootScope, '$broadcast')
+  };
+
+  mockScope = {
+    dialog: {close: () => {}}
+  };
+  stubScope = {
+    dialog: {
+      close: sinon.stub(mockScope.dialog, 'close')
+    }
+  };
+  return new ControllerStatic(mockRootScope, mockScope);
+})();
+var Definition = AddLatentVariable.Definition;
 
 describe('DialogAddLatentVariable', () => {
-  it('Runnable', () => {
-    assert(true === true);
+  it('Controller has loaded', () => {
+    assert(ControllerStatic !== void 0);
   });
+
+  it('Definition has loaded', () => {
+    assert(Definition !== void 0);
+  });
+
+  describe('Controller', () => {
+    describe('#add()', () => {
+      var dummy = 'dummy';
+      before(() => {
+        Controller.add(dummy);
+      });
+
+      it('should give the event name to arg[0] of $broadcast()', () => {
+        assert(stubRootScope.$broadcast.getCall(0).args[0] === 'isem:addVariable');
+      });
+
+      it('should give the value to arg[1] of $broadcast()', () => {
+        assert(stubRootScope.$broadcast.getCall(0).args[1] === dummy);
+      });
+
+      it('should do close()', () => {
+        assert(stubScope.dialog.close.callCount === 1);
+      });
+    });
+  });
+
+  describe('Definition', () => {
+    describe('.ddo()', () => {
+      it('should return the field of require including ^cwModal', () => {
+        assert(Definition.ddo().require === '^cwModal');
+      });
+
+      it('should return the field of controllerAs including AddLatentVariable', () => {
+        assert(Definition.ddo().controllerAs === 'AddLatentVariable');
+      });
+    });
+  });
+
 });
