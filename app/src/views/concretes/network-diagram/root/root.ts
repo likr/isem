618d109ee845fe6a9e5200a8c20ec54d7fbbed7c
armorik83 <okunokentaro@me.com>
2015-02-27 13:59:45 +0900

    refactor(network-diagram): wrap with a class of Definition

--- app/src/views/concretes/network-diagram/root/root.ts
+++ app/src/views/concretes/network-diagram/root/root.ts
@@ -5,17 +5,17 @@ import app = require('../../../../scripts/app');
 import vas = require('../../../../scripts/modules/variable-array-store');
 var Store: vas.IVariableArrayStore = vas.singleton;
 
-interface NetworkDiagramScope extends ng.IScope {
+interface Scope extends ng.IScope {
   _variableArray: string[];
 }
 
-class NetworkDiagramController {
+export class Controller {
   /**
    * @constructor
    * @ngInject
    */
   constructor(
-    private $scope: NetworkDiagramScope
+    private $scope: Scope
   ) {
     this.subscribe();
   }
@@ -25,42 +25,48 @@ class NetworkDiagramController {
    */
   private subscribe() {
     Store.init();
-    Store.addChangeListener((_, __) => {
-      this.$scope._variableArray = Store.variableArray;
-    });
+    Store.addChangeListener(this.changeCallback.bind(this));
+  }
+
+  /**
+   * @returns {void}
+   */
+  private changeCallback() {
+    this.$scope._variableArray = Store.variableArray;
   }
 }
 
-function styling(tElement: ng.IAugmentedJQuery) {
-  tElement
-    .addClass('container-fluid')
-    .css({
-      position: 'absolute',
-      top: '5em',
-      'overflow-y': 'scroll'
-    }).css({
-      width: '100%',
-      height: app.styles.mainDisplay.height
-    }).css({
-      // for looks, e.g. color, background-color...
-    });
-}
+export class Definition {
+  static styling(tElement: ng.IAugmentedJQuery) {
+    tElement
+      .addClass('container-fluid')
+      .css({
+        position: 'absolute',
+        top: '5em',
+        'overflow-y': 'scroll'
+      }).css({
+        width: '100%',
+        height: app.styles.mainDisplay.height
+      }).css({
+        // for looks, e.g. color, background-color...
+      });
+  }
 
-function compile(tElement: ng.IAugmentedJQuery, tAttrs: ng.IAttributes, _: any) {
-  styling(tElement);
-  return () => {}; // link is do nothing
-}
+  static compile(tElement: ng.IAugmentedJQuery, tAttrs: ng.IAttributes, _: any) {
+    Definition.styling(tElement);
+    return () => {}; // link is do nothing
+  }
 
-function ddo() {
-  return {
-    compile: compile,
-    controller: NetworkDiagramController,
-    controllerAs: 'NetworkDiagram',
-    restrict: 'E',
-    scope: {}, // use isolate scope and non interface
-    templateUrl: app.viewsDir.networkDiagram + 'root/root.html'
+  static ddo() {
+    return {
+      compile: Definition.compile,
+      controller: Controller,
+      controllerAs: 'NetworkDiagramController',
+      restrict: 'E',
+      scope: {}, // use isolate scope and non interface
+      templateUrl: app.viewsDir.networkDiagram + 'root/root.html'
+    }
   }
 }
 
-angular.module(app.appName).directive('isemNetworkDiagram', ddo);
-export = NetworkDiagramController;
+angular.module(app.appName).directive('isemNetworkDiagram', Definition.ddo);
