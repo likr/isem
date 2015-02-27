cd137c92d1bc2148d67a646e4a4139319a93df45
armorik83 <okunokentaro@me.com>
2015-02-27 17:04:24 +0900

    chore(mocks/angular): add mock rootScope

--- test/mocks/browser/angular.js
+++ test/mocks/browser/angular.js
@@ -2,17 +2,38 @@
 var sinon = require('sinon');
 var Injector = require('../../../app/src/scripts/injector');
 
-function mockAngular() {
-  // do nothing
-}
-
+function mockAngular() {}
 mockAngular.module = () => {
   return {
     config: () => {},
     directive: () => {}
   };
 };
+mockAngular.element = () => {};
+
+function mockElements() {}
+mockElements.eq = () => {};
+
+function mockRootElement() {}
+mockRootElement.scope = () => {};
+
+function mockRootScope() {}
+mockRootScope.$on = () => {};
 
 sinon.stub(Injector, 'angular').returns(mockAngular);
 
-module.exports.mock = mockAngular;
+module.exports.stubAngular = {
+  element: sinon.stub(mockAngular, 'element').withArgs('.ng-scope').returns(mockElements)
+};
+
+module.exports.stubElements = {
+  eq: sinon.stub(mockElements, 'eq').withArgs(0).returns(mockRootElement)
+};
+
+module.exports.stubRootElement = {
+  scope: sinon.stub(mockRootElement, 'scope').returns(mockRootScope)
+};
+
+module.exports.stubRootScope = {
+  $on: sinon.stub(mockRootScope, '$on')
+};
