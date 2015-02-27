9eea8c808352e6808b9ebb6c667ea3d963c103ed
armorik83 <okunokentaro@me.com>
2015-02-28 00:37:02 +0900

    refactor(csv-to-alpha-converter): divide

--- app/src/scripts/modules/csv-to-alpha-converter.ts
+++ app/src/scripts/modules/csv-to-alpha-converter.ts
@@ -15,28 +15,35 @@ class CsvToAlphaConverter {
    * @params {*} data
    * @returns {*}
    */
-  convert(data: any): any {
-    var observedVariables = ((data: any) => {
-      var vars: string[] = [];
-      var v: string;
-      for (v in data) {
-        if (data.hasOwnProperty(v)) {
-          vars.push(v);
-        }
-      }
-      return vars;
-    })(data[0]);
+  convert(data: any[]): {nodes: string[]; S: number[][];} {
+    var nodes = this.makeNodes(data[0]);
+    var S = this.makeS(data, nodes);
+    return {nodes: nodes, S: S};
+  }
 
-    var S = ((vars: string[], data: any) => {
-      var x = vars.map((key: string): number[] => {
-        return data.map((d: {[key: string]: number}): number => {
-          return d[key];
-        });
-      });
-      return semjs.stats.cov(x);
-    })(observedVariables, data);
+  /**
+   * @param {*} data
+   * @returns {string[]}
+   */
+  private makeNodes(data: any): string[] {
+    var vars: string[] = [];
+    var v: string;
+    for (v in data) {
+      if (data.hasOwnProperty(v)) {vars.push(v);}
+    }
+    return vars;
+  }
 
-    return {nodes: observedVariables, S: S};
+  /**
+   * @param {*} data
+   * @param {string[]} vars
+   * @returns {string[]}
+   */
+  private makeS(data: any, vars: string[]): number[][] {
+    var x = vars.map((key: string): number[] => {
+      return data.map((d: {[key: string]: number}): number => d[key]);
+    });
+    return semjs.stats.cov(x);
   }
 }
 
