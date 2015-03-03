f67ea43f4f91f641d158f6d56837dd16cd289bfc
armorik83 <okunokentaro@me.com>
2015-03-03 11:45:12 +0900

    test(mocks/network-diagram-dispatcher): fix a missing stub

--- test/mocks/isem/network-diagram-dispatcher.js
+++ test/mocks/isem/network-diagram-dispatcher.js
@@ -3,12 +3,14 @@ var sinon = require('sinon');
 var IsemInjector = require('../../../app/src/scripts/isem-injector');
 
 function mock() {}
+mock.prototype.onAddRelation = () => {};
 mock.prototype.onAddVariable = () => {};
 mock.prototype.onImportFile = () => {};
 
 sinon.stub(IsemInjector, 'NetworkDiagramDispatcher').returns(mock.prototype);
 
 var stub = {
+  onAddRelation: sinon.stub(mock.prototype, 'onAddRelation'),
   onAddVariable: sinon.stub(mock.prototype, 'onAddVariable'),
   onImportFile:  sinon.stub(mock.prototype, 'onImportFile')
 };
