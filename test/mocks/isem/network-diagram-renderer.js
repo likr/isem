28681a34b956ac9e53252744573e4a555b672e6f
armorik83 <okunokentaro@me.com>
2015-03-06 15:37:13 +0900

    test(test): update

--- test/mocks/isem/network-diagram-renderer.js
+++ test/mocks/isem/network-diagram-renderer.js
@@ -4,11 +4,13 @@ var IsemInjector = require('../../../app/src/scripts/isem-injector');
 
 function mock() {}
 mock.prototype.addListenerToClickAddRelationButton = () => {};
+mock.prototype.addListenerToClickVertex = () => {};
 
 sinon.stub(IsemInjector, 'NetworkDiagramRenderer').returns(mock.prototype);
 
 var stub = {
-  addListenerToClickAddRelationButton: sinon.stub(mock.prototype, 'addListenerToClickAddRelationButton')
+  addListenerToClickAddRelationButton: sinon.stub(mock.prototype, 'addListenerToClickAddRelationButton'),
+  addListenerToClickVertex:            sinon.stub(mock.prototype, 'addListenerToClickVertex')
 };
 
 module.exports = {
