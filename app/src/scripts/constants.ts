d6892f49896b6a41b2c0c184e9a2d51011faf391
armorik83 <okunokentaro@me.com>
2015-03-08 02:13:18 +0900

    feat(sub-tool-group): toggle all

--- app/src/scripts/constants.ts
+++ app/src/scripts/constants.ts
@@ -1,10 +1,13 @@
 'use strict';
 
 var prefix = 'isem:';
-export var ADD_LATENT_VARIABLE   = prefix + 'ADD_LATENT_VARIABLE';
-export var ADD_RELATION          = prefix + 'ADD_RELATION';
-export var IMPORT_FILE           = prefix + 'IMPORT_FILE';
-export var REDRAW_DIAGRAM        = prefix + 'REDRAW_DIAGRAM';
-export var REMOVE_RELATION       = prefix + 'REMOVE_RELATION';
-export var TOGGLE_VERTEX_DISPLAY = prefix + 'TOGGLE_VERTEX_DISPLAY';
-export var UPDATE_DIAGRAM        = prefix + 'UPDATE_DIAGRAM';
+export var ADD_LATENT_VARIABLE = prefix + 'ADD_LATENT_VARIABLE';
+export var ADD_RELATION        = prefix + 'ADD_RELATION';
+export var IMPORT_FILE         = prefix + 'IMPORT_FILE';
+export var REDRAW_DIAGRAM      = prefix + 'REDRAW_DIAGRAM';
+export var REMOVE_RELATION     = prefix + 'REMOVE_RELATION';
+export var UPDATE_DIAGRAM      = prefix + 'UPDATE_DIAGRAM';
+
+export var DISABLE_VERTEX_DISPLAY = prefix + 'DISABLE_VERTEX_DISPLAY';
+export var ENABLE_VERTEX_DISPLAY  = prefix + 'ENABLE_VERTEX_DISPLAY';
+export var TOGGLE_VERTEX_DISPLAY  = prefix + 'TOGGLE_VERTEX_DISPLAY';
