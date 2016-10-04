9719de036af2e87536614dc5374f74b151545c38
OKUNOKENTARO <okunokentaro@me.com>
2016-10-05 07:58:04 +0900

    fix styles

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,11 +7,11 @@ import {AppStore} from './app.store'
   template: `
     <style>
       .header {
-        background-color: #455A64
-        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.26)
-        height: 64px
-        line-height: 64px
-        color: #FFFFFF
+        background-color: #455A64;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.26);
+        height: 64px;
+        line-height: 64px;
+        color: #FFFFFF;
       }
     </style>
     <div class="header">
