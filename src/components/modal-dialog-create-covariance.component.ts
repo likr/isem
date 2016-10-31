73351e49d4a94e3c0170dc4559812a93625fff72
OKUNOKENTARO <okunokentaro@me.com>
2016-10-31 18:31:34 +0900

    add modal dialogs

---
+++ src/components/modal-dialog-create-covariance.component.ts
@@ -0,0 +1,54 @@
+import {Component} from '@angular/core'
+
+import {css as ModalDialogCss} from './modal-dialog.component'
+import {AbstractComponent} from './abstract'
+import {AppDispatcher} from '../application/app'
+import {ModalDialogActions} from '../application/modal-dialog'
+import {ProjectsStore} from '../application/project'
+import {LatentVariableVM} from '../application/variable'
+import {ProjectsActions} from '../application/project/projects.actions'
+
+@Component({
+  selector : 'is-modal-dialog-create-covariance',
+  styleUrls: ['src/shared-styles/modal-dialog.css'],
+  template : `
+    <style>
+      .buttons {
+        position: absolute;
+        bottom: ${ModalDialogCss.bodyPadding};
+        right:  ${ModalDialogCss.bodyPadding};
+      }
+    </style>
+
+    <h2>{{'ModalDialogCreateCovariance.Header' | translate}}</h2>
+
+    <div class="buttons">
+      <is-ui-button
+        [label]="'OK' | translate"
+        [type] ="'primary'"
+        (clickButton)="onClickPrimary($event)"
+      ></is-ui-button>
+    </div>
+  `
+})
+export class ModalDialogCreateCovariance extends AbstractComponent {
+
+  private variable: LatentVariableVM
+  private newKey: string
+
+  constructor(private modalDialog: ModalDialogActions,
+              private projects: ProjectsActions,
+              private dispatcher: AppDispatcher,
+              private store: ProjectsStore) {
+    super()
+  }
+
+  ngOnInit() {
+    //
+  }
+
+  onClickPrimary() {
+    //
+  }
+
+}
