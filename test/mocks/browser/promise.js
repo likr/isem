de1d0512227021ea9fc7bd67eb0894224fde4ee9
Yosuke ONOUE <onoue@likr-lab.com>
2016-12-07 15:11:24 +0900

    Erase all !

--- test/mocks/browser/promise.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-import sinon from 'sinon';
-import injector from '../../../app/src/scripts/injector';
-
-/* stubs */
-sinon.stub(injector, 'Promise').returns(Promise);
--- test/utils.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-export function resetAll(stubs) {
-  Object.keys(stubs).forEach(v => stubs[v].reset());
-}
-
-export function restoreAll(stubs) {
-  Object.keys(stubs).forEach(v => stubs[v].restore());
-}
