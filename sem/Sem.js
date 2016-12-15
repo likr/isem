894e69a9e9857c592a2ca3447695f76c567871af
ash1day <y4ashida@gmail.com>
2016-12-15 18:34:06 +0900

    Add hide flags to viz components

--- sem/Sem.js
+++ sem/Sem.js
@@ -4,15 +4,30 @@ import Fit from './Fit'
 import Covariance from './Covariance'
 import TotalEffect from './TotalEffect'
 
+const defaultHide = { graph: false, fit: false, covariance: false, totalEffect: false }
+
 class Sem extends Component {
   render () {
-    return <div className='Sem' style={{position: 'relative', height: '100%'}}>
-      <Graph json={this.props.json} />
-      <Fit goodness_of_fit={this.props.json.goodness_of_fit} />
+    let { json, hide } = this.props
+    if (hide === undefined) hide = defaultHide
+
+    const vizComponents = []
+
+    if (!hide.graph) vizComponents.push(<Graph json={json} />)
+    if (!hide.fit)   vizComponents.push(<Fit goodness_of_fit={json.goodness_of_fit} />)
+
+    const matrixComponents = []
+    if (!hide.covariance)  matrixComponents.push(<Covariance json={json} />)
+    if (!hide.totalEffect) matrixComponents.push(<TotalEffect json={json} />)
+
+    vizComponents.push(
       <div style={{ overflow: 'scroll', position: 'absolute', right: 0, top: 0, height: '100%' }} >
-        <Covariance json={this.props.json} />
-        <TotalEffect json={this.props.json} />
+        { matrixComponents }
       </div>
+    )
+
+    return <div className='Sem' style={{ position: 'relative', height: '100%' }}>
+      { vizComponents }
     </div>
   }
 }
