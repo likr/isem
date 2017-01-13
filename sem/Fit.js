febad3a2fafe741512faf086f3007a07db6b4f5b
Yoshihiro Ashida <ash1day@Leskun.local>
2017-01-13 15:23:12 +0900

    Color fits' background if the values indicate fair fit or poor fit

--- sem/Fit.js
+++ sem/Fit.js
@@ -1,6 +1,33 @@
 import React, {Component} from 'react'
 
-const fitsKeyNames = [ 'gfi', 'agfi', 'rmr', 'cfi', 'aic', 'rmsea' ]
+const fitsConds = {
+  gfi: { greaterThan: 0.9 },
+  agfi: { greaterThan: 0.9 },
+  cfi: { greaterThan: 0.9 },
+  rmr: {},
+  rmsea: { lessThan: 0.1 },
+  aic: {}
+}
+
+const isGood = (name, val) => {
+  if ('greaterThan' in fitsConds[name]) {
+    return (val >= fitsConds[name].greaterThan)
+  }
+  if ('lessThan' in fitsConds[name]) {
+    return (val <= fitsConds[name].lessThan)
+  }
+}
+
+const fitColor = (name, val) => {
+  switch (isGood(name, val)) {
+    case true:
+      return '#1abc9c'
+    case false:
+      return '#e74c3c'
+    default:
+      return '#2c3e50'
+  }
+}
 
 class Fit extends Component {
   render () {
@@ -9,34 +36,22 @@ class Fit extends Component {
 
     if (!fits) return <div className='Fit' />
 
-    const style = {
-      tdLeft: {
-        textAlign: 'right'
-      },
-      tdRight: {
-        textAlign: 'left'
-      }
-    }
-
-    for (const k of Object.keys(fits).sort()) {
-      if (fitsKeyNames.includes(k)) {
-        rows.push(
-          <tr key={k} >
-            <th>{k.toUpperCase()}</th>
-            <td style={style.tdLeft} >{fits[k].split('.')[0]}</td>
-            <td style={style.tdRight} >.{fits[k].split('.')[1]}</td>
-          </tr>
-        )
-      }
+    for (const fitKeyName in fitsConds) {
+      rows.push(
+        <tr key={fitKeyName} style={{ background: fitColor(fitKeyName, fits[fitKeyName]), padding: '1em' }} >
+          <th style={{ padding: '0.2em' }}>{fitKeyName.toUpperCase()}</th>
+          <td style={{ textAlign: 'right', padding: '0.2em 0 0.2em 0.2em' }} >{fits[fitKeyName].split('.')[0]}</td>
+          <td style={{ textAlign: 'left', padding: '0.2em 0.2em 0.2em 0' }} >.{fits[fitKeyName].split('.')[1]}</td>
+        </tr>
+      )
     }
 
     return <div className='Fit' style={{
       position: 'absolute',
       left: 0,
-      bottom: 0,
-      backgroundColor: 'rgba(245, 245, 245, 0.8)'
+      bottom: 0
     }}>
-      <table>
+      <table style={{ color: 'white' }}>
         <tbody>{rows}</tbody>
       </table>
     </div>
