ac9d703b63183ed9227e6d57c2dca429baf7b510
Yosuke ONOUE <onoue@likr-lab.com>
2017-07-12 08:50:03 +0900

    (re)enable production mode

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,8 @@ import {enableProdMode} from '@angular/core'
 import {environment} from './environments/environment'
 import {AppModule} from './app/app.module'
 
+ if (environment.production) {
+   enableProdMode()
+ }
+
 platformBrowserDynamic().bootstrapModule(AppModule)
